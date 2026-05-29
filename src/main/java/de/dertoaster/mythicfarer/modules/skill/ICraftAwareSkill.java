abfef60cbdb8cd95197d26d52a8600058db6d344
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2026-05-29 16:27:27 +0200

    refactor Craft logic into own interface

---
+++ src/main/java/de/dertoaster/mythicfarer/modules/skill/ICraftAwareSkill.java
@@ -0,0 +1,42 @@
+package de.dertoaster.mythicfarer.modules.skill;
+
+import de.dertoaster.mythicfarer.util.CraftUtil;
+import io.lumine.mythic.api.adapters.AbstractEntity;
+import io.lumine.mythic.api.skills.SkillMetadata;
+import io.lumine.mythic.api.skills.SkillResult;
+import net.countercraft.movecraft.MovecraftLocation;
+import net.countercraft.movecraft.craft.Craft;
+import net.countercraft.movecraft.craft.CraftManager;
+import org.bukkit.entity.Entity;
+
+import javax.annotation.Nullable;
+import java.util.UUID;
+
+public interface ICraftAwareSkill {
+
+    // Return wether or not to check our own craft
+    boolean checkOwnedCraft();
+    // Return wether or not to check a craft of which we are a passenger
+    boolean checkPassengerCraft();
+
+    SkillResult castAtCraft(SkillMetadata skillMetadata, AbstractEntity entityTarget, Craft craftTarget);
+
+    @Nullable
+    public default Craft getCraftByMythicMob(final AbstractEntity abstractEntity) {
+        Entity bukkitEntity = abstractEntity.getBukkitEntity();;
+        if (bukkitEntity != null) {
+            Craft craft = null;
+            if (this.checkOwnedCraft()) {
+                craft = CraftManager.getInstance().getCraftByEntity(bukkitEntity);
+            }
+            if (craft == null && this.checkPassengerCraft()) {
+                final MovecraftLocation location = new MovecraftLocation(abstractEntity.getLocation().getBlockX(), abstractEntity.getLocation().getBlockY(), abstractEntity.getLocation().getBlockZ());
+                final UUID worldUUID = abstractEntity.getWorld().getUniqueId();
+                craft = CraftUtil.fastFindCraftAt(location, worldUUID, true);
+            }
+            return craft;
+        }
+        return null;
+    }
+
+}
--- src/main/java/de/dertoaster/mythicfarer/modules/skill/ITargetedCraftSkill.java
+++ src/main/java/de/dertoaster/mythicfarer/modules/skill/ITargetedCraftSkill.java
@@ -1,33 +1,18 @@
 package de.dertoaster.mythicfarer.modules.skill;
 
-import de.dertoaster.mythicfarer.util.CraftUtil;
 import io.lumine.mythic.api.adapters.AbstractEntity;
 import io.lumine.mythic.api.skills.INoTargetSkill;
 import io.lumine.mythic.api.skills.SkillMetadata;
 import io.lumine.mythic.api.skills.SkillResult;
-import net.countercraft.movecraft.MovecraftLocation;
 import net.countercraft.movecraft.craft.Craft;
-import net.countercraft.movecraft.craft.CraftManager;
-import org.bukkit.entity.Entity;
 
-import java.util.UUID;
-
-public interface ITargetedCraftSkill extends INoTargetSkill {
+public interface ITargetedCraftSkill extends INoTargetSkill, ICraftAwareSkill {
 
     @Override
     default SkillResult cast(SkillMetadata skillMetadata) {
         final AbstractEntity abstractEntity = skillMetadata.getCaster().getEntity();
-        Entity bukkitEntity = abstractEntity.getBukkitEntity();;
-        if (bukkitEntity != null) {
-            Craft craft = null;
-            if (this.checkOwnedCraft()) {
-                craft = CraftManager.getInstance().getCraftByEntity(bukkitEntity);
-            }
-            if (craft == null && this.checkPassengerCraft()) {
-                final MovecraftLocation location = new MovecraftLocation(abstractEntity.getLocation().getBlockX(), abstractEntity.getLocation().getBlockY(), abstractEntity.getLocation().getBlockZ());
-                final UUID worldUUID = abstractEntity.getWorld().getUniqueId();
-                craft = CraftUtil.fastFindCraftAt(location, worldUUID, true);
-            }
+        if (abstractEntity != null) {
+            final Craft craft = this.getCraftByMythicMob(abstractEntity);
             if (craft != null) {
                 return castAtCraft(skillMetadata, abstractEntity, craft);
             }
@@ -35,10 +20,4 @@ public interface ITargetedCraftSkill extends INoTargetSkill {
         return SkillResult.INVALID_TARGET;
     }
 
-    // Return wether or not to check our own craft
-    boolean checkOwnedCraft();
-    // Return wether or not to check a craft of which we are a passenger
-    boolean checkPassengerCraft();
-
-    SkillResult castAtCraft(SkillMetadata skillMetadata, AbstractEntity entityTarget, Craft craftTarget);
 }
