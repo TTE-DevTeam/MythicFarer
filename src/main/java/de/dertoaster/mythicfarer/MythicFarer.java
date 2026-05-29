6ac963b58e33e9f50a869008fd869808756d8c76
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2026-05-29 12:42:00 +0200

    remove unnecessary code

--- src/main/java/de/dertoaster/mythicfarer/MythicFarer.java
+++ src/main/java/de/dertoaster/mythicfarer/MythicFarer.java
@@ -3,8 +3,6 @@ package de.dertoaster.mythicfarer;
 import de.dertoaster.modulecore.AbstractModularPlugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.logging.Level;
-
 public final class MythicFarer extends AbstractModularPlugin<AbstractMythicFarerModule> {
 
     // Initialize INSTANCE field
@@ -16,26 +14,6 @@ public final class MythicFarer extends AbstractModularPlugin<AbstractMythicFarer
 
     public static JavaPlugin getInstance() {
         return INSTANCE;
-    }
-
-    @Override
-    public void onEnable() {
-        setLogLevel(Level.INFO);
-        super.onEnable();
-    }
-
-    private void setLogLevel(Level defaultLevel) {
-        Level logLevel = defaultLevel;
-        if (getConfig().contains("LogLevel")) {
-            String configLL = getConfig().getString("LogLevel", defaultLevel.getName());
-            try {
-                logLevel = Level.parse(configLL);
-            } catch(Exception ex) {
-                this.getLogger().warning("Invalid loglevel <" + configLL + "> entered!");
-                logLevel = defaultLevel;
-            }
-        }
-        this.getLogger().setLevel(logLevel);
     }
 
     @Override
